2ff681425b017e5c21ffef8997c548fc5b93aac0
Piyush Manoj Jain <165635032+PiyushJain045@users.noreply.github.com>
2025-06-04 23:19:31 +0530

    Update settings.py

--- AiForm_core/settings.py
+++ AiForm_core/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+import os
 
+# Tell Django where to store collected static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
